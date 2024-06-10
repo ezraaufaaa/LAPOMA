698ea157c1fc070d9e1de190c4cec69c1df9287f
Ezra Aufaa Malefa <ezra.harjanto@gmail.com>
2024-06-10 23:33:19 +0700

    Add Project
    -

---
+++ README.md
@@ -0,0 +1,8 @@
+# Website Pengaduan Masyarakat Kota Malang | LAKOMA
+
+<br> ## Administrator
+<br> url: http://localhost/lapoma/auth
+<br> username: admin
+<br> password: admin
+<br> 
+<br
---
+++ application/views/templates/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" class="h-100" id="page-top">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="description" content="LAPOMA adalah website pengaduan pelayanan publik di Kota Malang, sebuah platform yang dirancang untuk memudahkan warga Kota Malang dalam mengomunikasikan tujuan, rekomendasi, keluhan, dan pertanyaan mereka kepada perwakilan pemerintah kota.">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="google-site-verification" content="https://maps.app.goo.gl/pmCJssrsUPuwapMP6" />
+  <?php header('Set-Cookie: cross-site-cookie=zona_lapor; SameSite=None; Secure'); ?>
+  <?php include 'include/js.php'; ?>
+  <?php include 'include/css.php'; ?>
+  <title><?= $title; ?></title>
+</head>
+
+<body class="d-flex flex-column h-100">        
+        
